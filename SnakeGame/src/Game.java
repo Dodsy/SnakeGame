16f9735f05cd98b5882838e7f1c32f64a76d2e19
Nate <74925479+Dodsy@users.noreply.github.com>
2021-08-23 12:10:55 -0400

    Add files via upload

--- SnakeGame/src/Game.java
+++ SnakeGame/src/Game.java
@@ -11,24 +11,28 @@ public class Game extends JFrame{
     final int RECTANGLE_HEIGHT = 10;
     final int rectangleCountX = 30;
     final int rectangleCountY = 30;
+
     private int current;
     private int vk;
     private int score = 0;
     private int high_score;
+
     private List<Shape> grid;
     private List<Shape> fill;
     private List<Shape> crums;
+
     private ArrayList<Integer> upper_bounds;
     private ArrayList<Integer> lower_bounds;
+
     private ArrayList<Integer> snake_list;
+
     private Crum crum = new Crum(rectangleCountX*rectangleCountY);
     PaintPane ppane;
-    public Game(){
 
+    public Game(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
         setResizable(false);
-
         ppane = new PaintPane();
         ppane.setBackground(Color.black);
         ppane.requestFocus();
@@ -41,6 +45,7 @@ public class Game extends JFrame{
     class PaintPane extends JPanel{
         public PaintPane(){
             
+            
 
             setPreferredSize(new Dimension(rectangleCountX*RECTANGLE_WIDTH,rectangleCountY*RECTANGLE_HEIGHT));
             setFocusable(true);
@@ -49,7 +54,11 @@ public class Game extends JFrame{
             grid = new ArrayList<>(rectangleCountX*rectangleCountY);
             fill = new ArrayList<>(rectangleCountX*rectangleCountY);
             crums = new ArrayList<>(rectangleCountX*rectangleCountY);
+
+
             snake_list = new ArrayList<>();
+
+
             upper_bounds = new ArrayList<>(rectangleCountX);
             lower_bounds = new ArrayList<>(rectangleCountX);
             for (int i = 0; i < rectangleCountX; i++){
@@ -73,7 +82,7 @@ public class Game extends JFrame{
                     } 
                     if (key == KeyEvent.VK_DOWN){
                         vk = -2;
-                    }
+                    } 
   
                 }
             });
@@ -117,8 +126,7 @@ public class Game extends JFrame{
     }
     public void check_collision(int current){
         if (snake_list.contains(current)){
-           collide();
-            
+           collide();  
         }
     }
     private void collide(){
@@ -133,39 +141,38 @@ public class Game extends JFrame{
         setTitle("High Score: "+high_score+" , Current Score: "+score);
         repaint();
     }
-    public void play(){
-        int start = rectangleCountX/2*rectangleCountY/2;
-        current = start;
-        fill.add(grid.get(start));
-        crums.add(grid.get(crum.get_location()));
-        setTitle("Dini's Snake Game");
-        repaint(); 
+    private void game_loop(){
         while(true){
+            int temp_head = current;
             if(vk == 1){
                 // go right
-                current = (current+rectangleCountY)%(rectangleCountX*rectangleCountY);
-                if (current > rectangleCountX*rectangleCountY){
-                    current = current - (rectangleCountX*rectangleCountY);
+                current = (current+rectangleCountY);
+                if (current > (rectangleCountX*rectangleCountY-1)){
+                    current = temp_head;
+                    collide();
                 }
  
             } else if (vk == -1){
                 // go left
-                current = (current-rectangleCountY)%(rectangleCountX*rectangleCountY);
+                current = (current-rectangleCountY);
                 if (current < 0){
-                    current = current + (rectangleCountX*rectangleCountY);
+                    current = temp_head;
+                    collide();
                 }
 
             } else if (vk == 2){
                 // go up
                 if (upper_bounds.contains(current)){
-                    current = current + rectangleCountY - 1;
+                    current = temp_head;
+                    collide();
                 } else {
                     current = current - 1;
                 }
             } else if (vk == -2){
                 // go down
                 if (lower_bounds.contains(current)){
-                    current = current - rectangleCountY + 1;
+                    current = temp_head;
+                    collide();
                 } else {
                     current = current + 1;
                 }
@@ -184,13 +191,20 @@ public class Game extends JFrame{
             }
             repaint();
             try {
-                TimeUnit.MILLISECONDS.sleep(125);
+                TimeUnit.MILLISECONDS.sleep(100);
             } catch (InterruptedException e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
-
+    }
+    public void play(){
+        int start = rectangleCountX/2*rectangleCountY/2;
+        current = start;
+        fill.add(grid.get(start));
+        crums.add(grid.get(crum.get_location()));
+        repaint();
+        game_loop(); 
     }
     public static void main(String[] args) {
         Game game = new Game();
